9ff1dc6615c73af74f411ba746134c185e64d690
Chris <chris79gp@outlook.com>
2024-09-07 17:42:44 +0100

    Refactor: abstract table creation to `schema.sql`

---
+++ db/schema.sql
@@ -0,0 +1,44 @@
+DROP TABLE IF EXISTS book_languages;
+DROP TABLE IF EXISTS book_genres;
+DROP TABLE IF EXISTS fact_books;
+DROP TABLE IF EXISTS dim_languages;
+DROP TABLE IF EXISTS dim_genres;
+DROP TABLE IF EXISTS dim_authors;
+
+CREATE TABLE IF NOT EXISTS dim_authors (
+  author_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  first_name VARCHAR(32),
+  last_name VARCHAR(32),
+  date_of_birth DATE,
+  nationality VARCHAR(32)
+);
+
+CREATE TABLE IF NOT EXISTS dim_genres (
+  genre_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  genre_name VARCHAR(32),
+  description TEXT
+);
+
+CREATE TABLE IF NOT EXISTS dim_languages (
+  language_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  language_name VARCHAR(32)
+);
+
+CREATE TABLE IF NOT EXISTS fact_books (
+  book_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  author_id INT REFERENCES dim_authors(author_id),
+  publication_year INT CHECK (publication_year <= EXTRACT(YEAR FROM CURRENT_DATE)),
+  category TEXT CHECK (category IN ('fiction', 'non-fiction'))
+);
+
+CREATE TABLE IF NOT EXISTS book_genres (
+  book_id INT REFERENCES fact_books(book_id),
+  genre_id INT REFERENCES dim_genres(genre_id),
+  PRIMARY KEY (book_id, genre_id)
+);
+
+CREATE TABLE IF NOT EXISTS book_languages (
+  book_id INT REFERENCES fact_books(book_id),
+  language_id INT REFERENCES dim_languages(language_id),
+  PRIMARY KEY (book_id, language_id)
+);
--- db/seed.js
+++ db/seed.js
@@ -1,50 +1,12 @@
 #! /usr/bin/env node
 
 const { Client } = require("pg");
+const fs = require("fs").promises;
+const path = require("path");
 require("dotenv").config();
 
 const isProduction = process.env.NODE_ENV === "production";
 const sslConfig = isProduction ? "?sslmode=require" : "";
-
-const SQL = `
-CREATE TABLE IF NOT EXISTS dim_authors (
-  author_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  first_name VARCHAR(32),
-  last_name VARCHAR(32),
-  date_of_birth DATE,
-  nationality VARCHAR(32)
-);
-
-CREATE TABLE IF NOT EXISTS dim_genres (
-  genre_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  genre_name VARCHAR(32),
-  description TEXT
-);
-
-CREATE TABLE IF NOT EXISTS dim_languages (
-  language_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  language_name VARCHAR(32)
-);
-
-CREATE TABLE IF NOT EXISTS fact_books (
-  book_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  author_id INT REFERENCES dim_authors(author_id),
-  publication_year INT CHECK (publication_year <= EXTRACT(YEAR FROM CURRENT_DATE)),
-  category TEXT CHECK (category IN ('fiction', 'non-fiction'))
-);
-
-CREATE TABLE IF NOT EXISTS book_genres (
-  PRIMARY KEY (book_id, genre_id),
-  book_id INT REFERENCES fact_books(book_id),
-  genre_id INT REFERENCES dim_genres(genre_id)
-);
-
-CREATE TABLE IF NOT EXISTS book_languages (
-  PRIMARY KEY (book_id, language_id),
-  book_id INT REFERENCES fact_books(book_id),
-  language_id INT REFERENCES dim_languages(language_id)
-);
-`;
 
 function getArgumentValue(flag, defaultValue) {
 	const index = process.argv.indexOf(flag);
@@ -52,7 +14,7 @@ function getArgumentValue(flag, defaultValue) {
 }
 
 async function main() {
-	console.log("seeding...");
+	console.log("Seeding...");
 
 	const user = getArgumentValue("--user", process.env.USER);
 	const password = getArgumentValue("--password", process.env.PASSWORD);
@@ -63,10 +25,15 @@ async function main() {
 		connectionString: `postgresql://${user}:${password}@${host}:5432/${database}${sslConfig}`,
 		ssl: isProduction ? { rejectUnauthorized: false } : false,
 	});
+
 	await client.connect();
-	await client.query(SQL);
+
+	const schema = await fs.readFile(path.join(__dirname, "schema.sql"), "utf8");
+	await client.query(schema);
+	console.log("Schema created successfully");
 	await client.end();
-	console.log("seeding complete");
+
+	console.log("Seeding complete");
 }
 
 main();
