07b4759504c76a8d0393af6a8c509fc599ee0350
Chris <chris79gp@outlook.com>
2024-09-29 22:57:36 +0100

    Feat: render error message on screen if author or genre(s) not found when submitting `#new-book` form

--- public/scripts/validateInputs.js
+++ public/scripts/validateInputs.js
@@ -9,17 +9,30 @@ export const validateAuthor = function (author, authors) {
 	const isInDatabase = checkInputInArray(author, authors);
 
 	if (!isInDatabase) {
-		return "Author not found";
+		return `Author ${author} not found.`;
 	}
 };
 
 export const validateGenres = function (genresInput, genres) {
-	const isInDatabase = genresInput
+	const genresArr = genresInput
 		.replace(/,\s*$/, "")
 		.split(",")
-		.every(input => checkInputInArray(input, genres));
+		.map(genre => genre.trim());
 
-	if (!isInDatabase) {
-		return "Genre(s) not found";
+	const notFoundGenres = genresArr.filter(input => !checkInputInArray(input, genres));
+
+	if (notFoundGenres.length > 0) {
+		let formattedGenres = "";
+
+		if (notFoundGenres.length === 2) {
+			formattedGenres += notFoundGenres.join(" and ");
+		} else if (notFoundGenres.length > 2) {
+			const lastGenre = notFoundGenres.pop();
+			formattedGenres += `${notFoundGenres.join(", ")}, and ${lastGenre}`;
+		} else {
+			formattedGenres += notFoundGenres[0];
+		}
+
+		return `Genre${notFoundGenres.length > 1 ? "s" : ""} ${formattedGenres} not found.`;
 	}
 };
