8a361b3174e55becd1bc01a8481a8645507dbcbf
Chris <chris79gp@outlook.com>
2024-10-07 21:52:30 +0100

    Feat: add `description` field to `#new-book-form` Add `description` argument to `Book` constructor Encapsulate contructor arguments in an object to negate need for positional arguments Update call to `Book` in `booksController.js` to reflect single-object argument and addition of `description` property Insert `description` property of `Book` into `book_description` column of `fact_books` table in `insertBooks.js`

--- models/Book.js
+++ models/Book.js
@@ -3,11 +3,12 @@ const { strToTitleCase } = require("../js/utils");
 const { capitaliseArray } = require("../js/utils");
 
 class Book {
-	constructor(title, authorID, genres, publicationYear) {
+	constructor({ title, description, author, genres, publicationYear }) {
 		this.title = strToTitleCase(title);
-		this.authorID = strToSlug(authorID);
+		this.authorID = strToSlug(author);
 		this.genres = capitaliseArray(genres.replace(/,\s*$/, "").split(","));
 		this.publicationYear = publicationYear || null;
+		this.description = description || null;
 	}
 }
 
