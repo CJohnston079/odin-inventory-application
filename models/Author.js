8fc2c2b1efac39cf749c94229555e0c19faa557b
Chris <chris79gp@outlook.com>
2024-10-14 21:38:25 +0100

    Refactor: use `strToNameCase()` to format `this.firstName` and `this.lastName` in `Author`

--- models/Author.js
+++ models/Author.js
@@ -1,11 +1,12 @@
 const { strToSlug } = require("../js/utils");
 const { strToTitleCase } = require("../js/utils");
+const { strToNameCase } = require("../js/utils");
 
 class Author {
 	constructor({ firstName, lastName, birthYear, nationality, biography }) {
 		this.slug = strToSlug(`${firstName} ${lastName}`);
-		this.firstName = strToTitleCase(firstName);
-		this.lastName = strToTitleCase(lastName);
+		this.firstName = strToNameCase(firstName);
+		this.lastName = strToNameCase(lastName);
 		this.nationality = strToTitleCase(nationality);
 		this.birthYear = birthYear || null;
 		this.biography = biography || null;
