6d6b40ac55f803e43d11e6ff6fbed6eacd54012c
Chris <chris79gp@outlook.com>
2024-10-27 09:24:39 +0000

    Feat: show more books by author on `book.ejs` page - Add `getRandomBooksByAuthor()` query to `queryBooks.js()`

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -53,10 +53,11 @@ exports.getNewBookForm = async function (req, res) {
 exports.getBook = async function (req, res) {
 	const bookId = req.params.book;
 	const book = (await db.books.getBook(bookId))[0];
+	const moreBooksByAuthor = await db.books.getRandomBooksByAuthor(book.author_id, 4);
 
 	book.genres = await db.genres.getGenresByBook(book.id);
 
-	res.render("./books/book", { title: book.book_title, book });
+	res.render("./books/book", { title: book.book_title, book, moreBooksByAuthor });
 };
 
 exports.updateBook = async function (req, res) {
