51a1e8b897a6f66f514eb6688493fe9d390de6d4
Chris <chris79gp@outlook.com>
2024-10-28 21:32:37 +0000

    Fix: add front-end validation to prevent submission of values that would violate `VARCHAR` constraint Resolves #60

--- public/scripts/form-handlers/input-handlers/validateName.js
+++ public/scripts/form-handlers/input-handlers/validateName.js
@@ -1,8 +1,15 @@
+import validateFieldLength from "./validateFieldLength.js";
 import { checkAuthorExists } from "../../validateInputs.js";
 import { strToNameCase } from "../../utils.js";
 
 const validateName = async function (firstNameInput, lastNameInput, currentName = null) {
-	const nameMessage = document.querySelector(`#${firstNameInput.id} ~ .field-message`);
+	const nameMessage = document.querySelector(`#${firstNameInput.id} ~ .field-message:last-child`);
+	const firstNameTooLong = !validateFieldLength(firstNameInput, { maxLength: 32 });
+	const lastNameTooLong = !validateFieldLength(lastNameInput, { maxLength: 32 });
+
+	if (firstNameTooLong || lastNameTooLong) {
+		return;
+	}
 
 	if (!firstNameInput.value || !lastNameInput.value) {
 		nameMessage.textContent = "";
